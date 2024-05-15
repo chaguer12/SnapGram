39b2b6a8bb41bfb03be39e68ef62ca8ad2020ce8
zoolotoui <chag.med.amine@gmail.com>
2024-05-15 17:08:36 +0100

    beginner freindly

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import {Routes, Route} from 'react-router-dom';
+import './globals.css';
+const App = () => {
+  return (
+    <main className='flex h-screen '>
+        <Routes>
+            {/* public routes*/}
+            {/* private routes*/}
+        </Routes>
+
+    </main>
+)
+}
+
+export default App
